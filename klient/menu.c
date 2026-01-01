e101c290855f3c44ac19a4b435dd47c5c56b6fdf
Ady38 <chobot.adam.2004@gmail.com>
2026-01-01 11:54:09 +0100

    add client-server communication functionality with socket implementation

--- klient/menu.c
+++ klient/menu.c
@@ -1,6 +1,13 @@
 #include "menu.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 38200
+#define BUFFER_SIZE 1024
 
 // Vytvorenie a zrusenie menu
 Menu* menu_vytvor(bool hra_pozastavena) {
@@ -16,13 +23,12 @@ void menu_zrus(Menu* menu) {
 // Funkcie menu
 void menu_nova_hra(Menu* menu) {
     printf("\n[Nova hra]\n");
+    // Spusti server v tomto procese
+    system("server/server &"); // spusti server na pozadi
+    sleep(1); // krátka pauza na inicializáciu servera
+    menu_pripojit_sa_k_hre(menu);
     // TODO: Vyber rezimu, typu sveta, nacitanie zo suboru, volba poctu hracov
     // menu->hra_pozastavena = false; // pripadne upravit stav
-}
-
-void menu_pripojit_sa_k_hre(Menu* menu) {
-    printf("\n[Pripojenie k hre]\n");
-    // TODO: Pripojenie k existujucej hre, pozastavenie pohybu hadikov na 3 sekundy
 }
 
 void menu_pokracovat_v_hre(Menu* menu) {
@@ -66,4 +72,54 @@ void menu_zobraz(Menu* menu) {
             printf("Neplatna volba. Skuste znova.\n");
         }
     }
+}
+
+void menu_pripojit_sa_k_hre(Menu* menu) {
+    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(client_fd < 0){
+        perror("Vytvorenie socketu zlyhalo");
+        return;
+    }
+    printf("Klient Socket vytvoreny\n");
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    if(inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
+        perror("Neplatna adresa");
+        close(client_fd);
+        return;
+    }
+
+    if(connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Pripojenie zlyhalo");
+        close(client_fd);
+        return;
+    }
+    printf("Pripojeny k serveru\n");
+    printf("Zadaj spravu (quit = koniec):\n");
+
+    char buffer[BUFFER_SIZE];
+    while(1) {
+        printf("> ");
+        fflush(stdout);
+        if(fgets(buffer, BUFFER_SIZE, stdin) == NULL){
+            break;
+        }
+        send(client_fd, buffer, strlen(buffer), 0);
+        if(strncmp(buffer, "quit", 4) == 0){
+            printf("Ukoncujem\n");
+            break;
+        }
+        memset(buffer, 0, BUFFER_SIZE);
+        int bytes_read = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
+        if(bytes_read <= 0){
+            printf("Server sa odpojil\n");
+            break;
+        }
+        printf("Echo zo servera: %s", buffer);
+    }
+    close(client_fd);
+    printf("Klient je ukonceny\n");
 }
--- server/server.c
+++ server/server.c
@@ -1,6 +1,87 @@
+#include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-int main(void) {
-    printf("Server is running...\n");
-    return 0;
+#define PORT 38200
+#define BUFFER_SIZE 1024
+
+int main()
+{
+  int server_fd;
+
+  //vytvorenie socketu
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if(server_fd < 0) {
+    perror("Chyba pri vytvarani socketu");
+    return -1;
+  }
+  printf("Server Socket bol vytvoreny\n");
+
+  int opt = 1;
+  setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+  //nastavenie ip adresy servera
+  struct sockaddr_in server_addr;
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(PORT); //host to network -> short / long
+
+  if(bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+    perror("Chyba pri priradeni adresy");
+    close(server_fd);
+    return -2;
+  }
+  printf("Socket naviazany na port: %d\n", PORT);
+
+  if(listen(server_fd, 5) < 0) {
+    perror("Listen zlyhal");
+    close(server_fd);
+    return -3;
+  }
+  printf("Cakam na pripojenie\n");
+
+  int client_fd;
+  struct sockaddr_in client_addr;
+  socklen_t client_len;
+
+  client_len = sizeof(client_addr);
+  client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
+
+  if(client_fd < 0) {
+    perror("Accept zlyhal");
+    return -4;
+  }
+  printf("Klient pripojeny %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+
+  char buffer[BUFFER_SIZE];
+  while(1) {
+    memset(buffer, 0, BUFFER_SIZE);
+
+    int bytes_read = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
+    if(bytes_read <= 0) {
+      printf("Klient sa odpojil\n");
+      break;
+    }
+    printf("Prijata sprava: %s", buffer);
+
+    if(strncmp(buffer, "quit", 4) == 0){
+      printf("Ukoncujem spojenie\n");
+      break;
+    }
+
+    send(client_fd, buffer, strlen(buffer), 0);
+  }
+
+
+  close(client_fd);
+  close(server_fd);
+  printf("Server je ukonceny\n");
+
+  return 0;
 }
