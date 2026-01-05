0556b8b2411764fb556a46b58341ef7824f2e9c2
Ady38 <chobot.adam.2004@gmail.com>
2026-01-05 13:02:36 +0100

    refactor CMake configuration and enhance menu functionality with threading support

--- klient/menu.c
+++ klient/menu.c
@@ -8,6 +8,7 @@
 #include <termios.h>
 #include <sys/select.h>
 #include <sys/time.h>
+#include <pthread.h>
 
 #define PORT 38200
 #define BUFFER_SIZE 1024
@@ -27,7 +28,7 @@ void menu_zrus(Menu* menu) {
 void menu_nova_hra(Menu* menu) {
     printf("\n[Nova hra]\n");
     // Spusti server v tomto procese
-    system("server/server &"); // spusti server na pozadi
+    system("./server &"); // spusti server na pozadi
     sleep(1); // krátka pauza na inicializáciu servera
     menu_pripojit_sa_k_hre(menu);
     // TODO: Vyber rezimu, typu sveta, nacitanie zo suboru, volba poctu hracov
@@ -92,9 +93,47 @@ void enable_raw_mode() {
     tcsetattr(STDIN_FILENO, TCSANOW, &raw);
 }
 
+void* menu_input_thread(void* arg) {
+    Menu* menu = (Menu*)arg;
+    char buffer[BUFFER_SIZE];
+    enable_raw_mode();
+    while (menu->running) {
+        int c = getchar();
+        if (c == EOF) break;
+        buffer[0] = (char)c;
+        buffer[1] = '\0';
+        send(menu->client_fd, buffer, 1, 0);
+        if (c == 'q' || c == 'Q') {
+            printf("\nUkoncujem\n");
+            menu->running = 0;
+            break;
+        }
+        system("clear");
+    }
+    disable_raw_mode();
+    return NULL;
+}
+
+void* menu_recv_thread(void* arg) {
+    Menu* menu = (Menu*)arg;
+    char buffer[BUFFER_SIZE];
+    while (menu->running) {
+        memset(buffer, 0, BUFFER_SIZE);
+        int bytes_read = recv(menu->client_fd, buffer, BUFFER_SIZE - 1, 0);
+        if (bytes_read <= 0) {
+            printf("\nServer sa odpojil\n");
+            menu->running = 0;
+            break;
+        }
+        buffer[bytes_read] = '\0';
+        printf("\rEcho zo servera: %s\n", buffer);
+    }
+    return NULL;
+}
+
 void menu_pripojit_sa_k_hre(Menu* menu) {
-    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if(client_fd < 0){
+    menu->client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(menu->client_fd < 0){
         perror("Vytvorenie socketu zlyhalo");
         return;
     }
@@ -106,59 +145,24 @@ void menu_pripojit_sa_k_hre(Menu* menu) {
     server_addr.sin_port = htons(PORT);
     if(inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
         perror("Neplatna adresa");
-        close(client_fd);
+        close(menu->client_fd);
         return;
     }
 
-    if(connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+    if(connect(menu->client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("Pripojenie zlyhalo");
-        close(client_fd);
+        close(menu->client_fd);
         return;
     }
     printf("Pripojeny k serveru\n");
     printf("Stlac klavesu (q = koniec):\n");
 
-    char buffer[BUFFER_SIZE];
-    enable_raw_mode();
-    fd_set readfds;
-    int running = 1;
-    while(running) {
-        FD_ZERO(&readfds);
-        FD_SET(STDIN_FILENO, &readfds);
-        FD_SET(client_fd, &readfds);
-        int maxfd = (STDIN_FILENO > client_fd ? STDIN_FILENO : client_fd) + 1;
-        int activity = select(maxfd, &readfds, NULL, NULL, NULL);
-        if(activity < 0) {
-            perror("Select zlyhal");
-            break;
-        }
-        // User input
-        if(FD_ISSET(STDIN_FILENO, &readfds)) {
-            int c = getchar();
-            if (c == EOF) break;
-            buffer[0] = (char)c;
-            buffer[1] = '\0';
-            send(client_fd, buffer, 1, 0);
-            if(c == 'q' || c == 'Q'){
-                printf("\nUkoncujem\n");
-                running = 0;
-                continue;
-            }
-            system("clear"); // clear only after sending a message
-        }
-        // Server message
-        if(FD_ISSET(client_fd, &readfds)) {
-            memset(buffer, 0, BUFFER_SIZE);
-            int bytes_read = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
-            if(bytes_read <= 0){
-                printf("\nServer sa odpojil\n");
-                break;
-            }
-            buffer[bytes_read] = '\0';
-            printf("\rEcho zo servera: %s\n", buffer);
-        }
-    }
-    disable_raw_mode();
-    close(client_fd);
+    menu->running = 1;
+    pthread_create(&menu->input_thread, NULL, menu_input_thread, menu);
+    pthread_create(&menu->recv_thread, NULL, menu_recv_thread, menu);
+    pthread_join(menu->input_thread, NULL);
+    pthread_join(menu->recv_thread, NULL);
+
+    close(menu->client_fd);
     printf("Klient je ukonceny\n");
 }
--- klient/menu.h
+++ klient/menu.h
@@ -2,10 +2,15 @@
 #define MENU_H
 
 #include <stdbool.h>
+#include <pthread.h>
 
 // "Objektová" štruktúra pre menu
 typedef struct Menu {
     bool hra_pozastavena;
+    int client_fd;
+    volatile int running;
+    pthread_t input_thread;
+    pthread_t recv_thread;
 } Menu;
 
 // Vytvorenie a zničenie menu
@@ -18,6 +23,10 @@ void menu_nova_hra(Menu* menu);
 void menu_pripojit_sa_k_hre(Menu* menu);
 void menu_pokracovat_v_hre(Menu* menu);
 void menu_koniec(Menu* menu);
+
+// Thread routines
+void* menu_input_thread(void* arg);
+void* menu_recv_thread(void* arg);
 
 #endif // MENU_H
 
